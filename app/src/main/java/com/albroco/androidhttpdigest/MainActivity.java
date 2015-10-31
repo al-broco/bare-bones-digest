7b43f5afc2a54b3b2d9968a94631fa4ce2fdd722
Petter Wintzell <al-broco@users.noreply.github.com>
2015-10-31 20:14:48 +0100

    Make (failing) request

--- app/src/main/java/com/albroco/androidhttpdigest/MainActivity.java
+++ app/src/main/java/com/albroco/androidhttpdigest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.albroco.androidhttpdigest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new MakeRequestAsyncTask().execute();
+    }
+
+    private static class MakeRequestAsyncTask extends AsyncTask<Void, Void, Void>  {
+        private static final String TEST_URL = "http://httpbin.org/basic-auth/user/passwd";
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL(TEST_URL).openConnection();
+                int statusCode = connection.getResponseCode();
+                Log.i(LOG_TAG, "Request response code: " + statusCode);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Request failed", e);
+            }
+
+            return null;
+        }
     }
 }
