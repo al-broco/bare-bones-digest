9458991ac90efa6deafdd00fc28e7d23b2852bbc
Petter Wintzell <al-broco@users.noreply.github.com>
2015-11-11 19:01:50 +0100

    Move augmented BNF parser to its own class

---
+++ androidhttpdigest/src/main/java/com/albroco/androidhttpdigest/Rfc2616AbnfParser.java
@@ -0,0 +1,189 @@
+package com.albroco.androidhttpdigest;
+
+public final class Rfc2616AbnfParser {
+  // TOOD: create table and do lookup
+  // Defined in RFC 2616, Section 2.2
+  private static final String ABNF_SEPARATOR_CHARACTERS = "()<>@,;:\\\"([]?={} \t";
+
+  private String input;
+  private int eltStart;
+  private int eltEnd;
+
+  public Rfc2616AbnfParser(String input) {
+    this.input = input;
+  }
+
+  public String get() {
+    return input.substring(eltStart, eltEnd);
+  }
+
+  public Rfc2616AbnfParser consumeLiteral(String literal) throws ParseException {
+    // Definition from RFC 2616, Section 2.1:
+    // "literal"
+    //    Quotation marks surround literal text. Unless stated otherwise,
+    //    the text is case-insensitive.
+    if (input.length() < eltEnd + literal.length()) {
+      throw new ParseException("Expected literal " + literal, this);
+    }
+
+    String substring = input.substring(eltEnd, eltEnd + literal.length());
+    if (!substring.equalsIgnoreCase(literal)) {
+      throw new ParseException("Expected literal " + literal, this);
+    }
+
+    eltStart = eltEnd;
+    eltEnd += literal.length();
+
+    return this;
+  }
+
+  public Rfc2616AbnfParser consumeWhitespace() throws ParseException {
+    // Definition from RFC 2616, Section 2.2:
+    // LWS            = [CRLF] 1*( SP | HT )
+    // CRLF           = CR LF
+    // CR             = <US-ASCII CR, carriage return (13)>
+    // LF             = <US-ASCII LF, linefeed (10)>
+    // SP             = <US-ASCII SP, space (32)>
+    // HT             = <US-ASCII HT, horizontal-tab (9)>
+    // TODO make more efficient
+    eltStart = eltEnd;
+    while (containsMoreData() && (input.charAt(eltEnd) == ' ' ||
+        input.charAt(eltEnd) == '\t' ||
+        input.charAt(eltEnd) == '\r' ||
+        input.charAt(eltEnd) == '\n')) {
+      ++eltEnd;
+    }
+
+    return this;
+  }
+
+  public Rfc2616AbnfParser consumeToken() throws ParseException {
+    // Definition from RFC 2616, Section 2.2:
+    // token          = 1*<any CHAR except CTLs or separators>
+    int tokenEnd = eltEnd;
+    while (tokenEnd < input.length() && isValidTokenChar(input.charAt(tokenEnd))) {
+      ++tokenEnd;
+    }
+
+    if (eltEnd == tokenEnd) {
+      throw new ParseException("Expected token", this);
+    }
+
+    eltStart = eltEnd;
+    eltEnd = tokenEnd;
+
+    return this;
+  }
+
+  private boolean isValidTokenChar(char c) {
+    // Definition from RFC 2616, Section 2.2:
+    // token          = 1*<any CHAR except CTLs or separators>
+    // CHAR           = <any US-ASCII character (octets 0 - 127)>
+    // CTL            = <any US-ASCII control character
+    //                  (octets 0 - 31) and DEL (127)>
+    if (c <= 31 || c > 126) {
+      return false;
+    }
+
+    return ABNF_SEPARATOR_CHARACTERS.indexOf(c) == -1;
+  }
+
+  public Rfc2616AbnfParser consumeQuotedString() throws ParseException {
+    // Definition from RFC 2616, Section 2.2:
+    // A string of text is parsed as a single word if it is quoted using
+    // double-quote marks.
+    //     quoted-string  = ( <"> *(qdtext | quoted-pair ) <"> )
+    //     qdtext         = <any TEXT except <">>
+    // The backslash character ("\") MAY be used as a single-character
+    // quoting mechanism only within quoted-string and comment constructs.
+    //     quoted-pair    = "\" CHAR
+    // TODO: Handle quoted characters
+    int stringEnd = eltEnd;
+    if (stringEnd >= input.length() || input.charAt(stringEnd) != '"') {
+      throw new ParseException("Expected quoted string", this);
+    }
+
+    stringEnd++;
+    while (stringEnd < input.length() && input.charAt(stringEnd) != '"') {
+      stringEnd++;
+    }
+    stringEnd++;
+
+    if (stringEnd > input.length()) {
+      throw new ParseException("Expected quoted string", this);
+    }
+
+    eltStart = eltEnd;
+    eltEnd = stringEnd;
+
+    return this;
+  }
+
+  public String getRemainingInput() {
+    return input.substring(eltEnd);
+  }
+
+  public boolean containsMoreData() {
+    return eltEnd < input.length();
+  }
+
+  public int getPos() {
+    return eltEnd;
+  }
+
+  /**
+   * Unqoutes a quoted string.
+   *
+   * Quoted strings are explained in
+   * <a href="https://tools.ietf.org/html/rfc2616#section-2.2">Section 2.2 of RFC 2616</a>.:
+   *
+   * <dl>
+   *     <dt>quoted-string</dt>
+   *     <dd>A string of text is parsed as a single word if it is quoted using
+   *         double-quote marks.
+   *         <pre>
+   * quoted-string  = ( &lt;"&gt; *(qdtext | quoted-pair ) &lt;"&gt; )
+   * qdtext         = &lt;any TEXT except &lt;"&gt;&gt;
+   *         </pre>
+   *         The backslash character ("\") MAY be used as a single-character
+   *         quoting mechanism only within quoted-string and comment
+   *         constructs.
+   *         <pre>
+   * quoted-pair    = "\" CHAR
+   *         </pre></dd>
+   * </dl>
+   *
+   * @param str the string to unqoute
+   * @return the unquoted string
+   */
+  public static String unquote(String str) {
+    // TODO: Handle malformed strings (missing quotes, strings ending in \)
+    if (str.indexOf('\\') == -1) {
+      return str.substring(1, str.length() - 1);
+    }
+
+    StringBuffer result = new StringBuffer();
+
+    int index = 1;
+    while (index < str.length() - 1) {
+      char c = str.charAt(index);
+      if (c == '\\') {
+        c = str.charAt(++index);
+      }
+      result.append(c);
+    }
+
+    return result.toString();
+  }
+
+  public static final class ParseException extends Exception {
+    ParseException(String message) {
+      super(message);
+    }
+
+    ParseException(String message, Rfc2616AbnfParser parser) {
+      this(message + " at pos " + parser.getPos() + ", remaining input: " +
+              parser.getRemainingInput());
+    }
+  }
+}
--- androidhttpdigest/src/main/java/com/albroco/androidhttpdigest/WwwAuthenticateHeader.java
+++ androidhttpdigest/src/main/java/com/albroco/androidhttpdigest/WwwAuthenticateHeader.java
@@ -1,10 +1,6 @@
 package com.albroco.androidhttpdigest;
 
 public class WwwAuthenticateHeader {
-  // TOOD: create table and do lookup
-  // Defined in RFC 2616, Section 2.2
-  private static final String EBNF_SEPARATOR_CHARACTERS = "()<>@,;:\\\"([]?={} \t";
-
   private static final String HTTP_DIGEST_CHALLENGE_PREFIX = "digest";
 
   private final String realm;
@@ -22,7 +18,7 @@ public class WwwAuthenticateHeader {
   }
 
   public static WwwAuthenticateHeader parse(String authenticateHeader) {
-    Parser parser = new Parser(authenticateHeader);
+    Rfc2616AbnfParser parser = new Rfc2616AbnfParser(authenticateHeader);
     try {
       parser.consumeLiteral(HTTP_DIGEST_CHALLENGE_PREFIX);
       parser.consumeWhitespace();
@@ -84,7 +80,7 @@ public class WwwAuthenticateHeader {
           // auth-param     = token "=" ( token | quoted-string )
           // TODO parse auth-params
           // TODO store (or ignore) auth-params
-          throw new ParseException("Unrecognized auth-param: " + token, parser);
+          throw new Rfc2616AbnfParser.ParseException("Unrecognized auth-param: " + token, parser);
         }
 
         parser.consumeWhitespace();
@@ -94,14 +90,14 @@ public class WwwAuthenticateHeader {
       }
 
       if (realm == null) {
-        throw new ParseException("Missing directive: realm");
+        throw new Rfc2616AbnfParser.ParseException("Missing directive: realm");
       }
       if (nonce == null) {
-        throw new ParseException("Missing directive: nonce");
+        throw new Rfc2616AbnfParser.ParseException("Missing directive: nonce");
       }
 
       return new WwwAuthenticateHeader(realm, nonce, opaqueQuoted, algorithm, stale);
-    } catch (ParseException e) {
+    } catch (Rfc2616AbnfParser.ParseException e) {
       return null;
     }
   }
@@ -234,193 +230,5 @@ public class WwwAuthenticateHeader {
    */
   public boolean isStale() {
     return stale;
-  }
-
-  /**
-   * Unqoutes a quoted string.
-   *
-   * Quoted strings are explained in
-   * <a href="https://tools.ietf.org/html/rfc2616#section-2.2">Section 2.2 of RFC 2616</a>.:
-   *
-   * <dl>
-   *     <dt>quoted-string</dt>
-   *     <dd>A string of text is parsed as a single word if it is quoted using
-   *         double-quote marks.
-   *         <pre>
-   * quoted-string  = ( &lt;"&gt; *(qdtext | quoted-pair ) &lt;"&gt; )
-   * qdtext         = &lt;any TEXT except &lt;"&gt;&gt;
-   *         </pre>
-   *         The backslash character ("\") MAY be used as a single-character
-   *         quoting mechanism only within quoted-string and comment
-   *         constructs.
-   *         <pre>
-   * quoted-pair    = "\" CHAR
-   *         </pre></dd>
-   * </dl>
-   *
-   * @param str the string to unqoute
-   * @return the unquoted string
-   */
-  public static String unquote(String str) {
-    // TODO: Handle malformed strings (missing quotes, strings ending in \)
-    if (str.indexOf('\\') == -1) {
-      return str.substring(1, str.length() - 1);
-    }
-
-    StringBuffer result = new StringBuffer();
-
-    int index = 1;
-    while (index < str.length() - 1) {
-      char c = str.charAt(index);
-      if (c == '\\') {
-        c = str.charAt(++index);
-      }
-      result.append(c);
-    }
-
-    return result.toString();
-  }
-
-  private static final class Parser {
-    private String input;
-    private int eltStart;
-    private int eltEnd;
-
-    public Parser(String input) {
-      this.input = input;
-    }
-
-    public String get() {
-      return input.substring(eltStart, eltEnd);
-    }
-
-    public Parser consumeLiteral(String literal) throws ParseException {
-      // Definition from RFC 2616, Section 2.1:
-      // "literal"
-      //    Quotation marks surround literal text. Unless stated otherwise,
-      //    the text is case-insensitive.
-      if (input.length() < eltEnd + literal.length()) {
-        throw new ParseException("Expected literal " + literal, this);
-      }
-
-      String substring = input.substring(eltEnd, eltEnd + literal.length());
-      if (!substring.equalsIgnoreCase(literal)) {
-        throw new ParseException("Expected literal " + literal, this);
-      }
-
-      eltStart = eltEnd;
-      eltEnd += literal.length();
-
-      return this;
-    }
-
-    public Parser consumeWhitespace() throws ParseException {
-      // Definition from RFC 2616, Section 2.2:
-      // LWS            = [CRLF] 1*( SP | HT )
-      // CRLF           = CR LF
-      // CR             = <US-ASCII CR, carriage return (13)>
-      // LF             = <US-ASCII LF, linefeed (10)>
-      // SP             = <US-ASCII SP, space (32)>
-      // HT             = <US-ASCII HT, horizontal-tab (9)>
-      // TODO make more efficient
-      eltStart = eltEnd;
-      while (containsMoreData() && (input.charAt(eltEnd) == ' ' ||
-          input.charAt(eltEnd) == '\t' ||
-          input.charAt(eltEnd) == '\r' ||
-          input.charAt(eltEnd) == '\n')) {
-        ++eltEnd;
-      }
-
-      return this;
-    }
-
-    public Parser consumeToken() throws ParseException {
-      // Definition from RFC 2616, Section 2.2:
-      // token          = 1*<any CHAR except CTLs or separators>
-      int tokenEnd = eltEnd;
-      while (tokenEnd < input.length() && isValidTokenChar(input.charAt(tokenEnd))) {
-        ++tokenEnd;
-      }
-
-      if (eltEnd == tokenEnd) {
-        throw new ParseException("Expected token", this);
-      }
-
-      eltStart = eltEnd;
-      eltEnd = tokenEnd;
-
-      return this;
-    }
-
-    private boolean isValidTokenChar(char c) {
-      // Definition from RFC 2616, Section 2.2:
-      // token          = 1*<any CHAR except CTLs or separators>
-      // CHAR           = <any US-ASCII character (octets 0 - 127)>
-      // CTL            = <any US-ASCII control character
-      //                  (octets 0 - 31) and DEL (127)>
-      if (c <= 31 || c > 126) {
-        return false;
-      }
-
-      return EBNF_SEPARATOR_CHARACTERS.indexOf(c) == -1;
-    }
-
-    public Parser consumeQuotedString() throws ParseException {
-      // Definition from RFC 2616, Section 2.2:
-      // A string of text is parsed as a single word if it is quoted using
-      // double-quote marks.
-      //     quoted-string  = ( <"> *(qdtext | quoted-pair ) <"> )
-      //     qdtext         = <any TEXT except <">>
-      // The backslash character ("\") MAY be used as a single-character
-      // quoting mechanism only within quoted-string and comment constructs.
-      //     quoted-pair    = "\" CHAR
-      // TODO: Handle quoted characters
-      int stringEnd = eltEnd;
-      if (stringEnd >= input.length() || input.charAt(stringEnd) != '"') {
-        throw new ParseException("Expected quoted string", this);
-      }
-
-      stringEnd++;
-      while (stringEnd < input.length() && input.charAt(stringEnd) != '"') {
-        stringEnd++;
-      }
-      stringEnd++;
-
-      if (stringEnd > input.length()) {
-        throw new ParseException("Expected quoted string", this);
-      }
-
-      eltStart = eltEnd;
-      eltEnd = stringEnd;
-
-      return this;
-    }
-
-    public String getRemainingInput() {
-      return input.substring(eltEnd);
-    }
-
-    public boolean containsMoreData() {
-      return eltEnd < input.length();
-    }
-
-    public int getPos() {
-      return eltEnd;
-    }
-
-    public String unquote(String s) {
-      return s.substring(1, s.length() - 1);
-    }
-  }
-
-  private static final class ParseException extends Exception {
-    ParseException(String message) {
-      super(message);
-    }
-
-    ParseException(String message, Parser parser) {
-      this(message + " at pos " + parser.getPos() + ", remaining input: " +
-          parser.getRemainingInput());
-    }
   }
 }
