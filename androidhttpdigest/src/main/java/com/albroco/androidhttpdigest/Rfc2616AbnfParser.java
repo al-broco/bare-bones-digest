c398c1d29c8fd24d21f1ea0520c2685825a457a8
Petter Wintzell <al-broco@users.noreply.github.com>
2015-11-11 19:20:12 +0100

    Wrote some javadoc

--- androidhttpdigest/src/main/java/com/albroco/androidhttpdigest/Rfc2616AbnfParser.java
+++ androidhttpdigest/src/main/java/com/albroco/androidhttpdigest/Rfc2616AbnfParser.java
@@ -1,9 +1,36 @@
 package com.albroco.androidhttpdigest;
 
+/**
+ * Parses strings with a grammar defined using the augmented BNF described
+ * in <a href="https://tools.ietf.org/html/rfc2616#section-2.1">Section 2.1
+ * of RFC 2616.</a>
+ *
+ * The parser is created with a string to parse as parameter:
+ * <p><blockquote><pre>
+ * String content = "realm = \"testrealm@host.com\""
+ * Rfc2616AbnfParser parser = new Rfc2616AbnfParser(content);
+ * </blockquote></pre>
+ *
+ * <p>Internally, the parser has a cursor position that can be advanced using
+ * the various <code>consume</code> methods. <code>consume</code> methods can
+ * be chained:
+ * <p><blockquote><pre>
+ * parser.consumeToken(); // Advances the cursor past "realm"
+ * parser.consumeWhitespace().consumeLiteral("=").consumeWhiteSpace();
+ *                        // Advances the cursor up to the first quote
+ * </blockquote></pre>
+ *
+ * <p>The string contents consumed by the latest call to a <code>consume</code>
+ * method can be obtained using {@link #get()}:
+ * <p><blockquote><pre>
+ * String realm = parser.consumeQuotedString().get();
+ * </blockquote></pre>
+ */
 public final class Rfc2616AbnfParser {
   // TOOD: create table and do lookup
   // Defined in RFC 2616, Section 2.2
-  private static final String ABNF_SEPARATOR_CHARACTERS = "()<>@,;:\\\"([]?={} \t";
+  private static final String ABNF_SEPARATOR_CHARACTERS =
+          "()<>@,;:\\\"([]?={} \t";
 
   private String input;
   private int eltStart;
@@ -17,7 +44,8 @@ public final class Rfc2616AbnfParser {
     return input.substring(eltStart, eltEnd);
   }
 
-  public Rfc2616AbnfParser consumeLiteral(String literal) throws ParseException {
+  public Rfc2616AbnfParser consumeLiteral(String literal) throws ParseException
+  {
     // Definition from RFC 2616, Section 2.1:
     // "literal"
     //    Quotation marks surround literal text. Unless stated otherwise,
@@ -61,7 +89,8 @@ public final class Rfc2616AbnfParser {
     // Definition from RFC 2616, Section 2.2:
     // token          = 1*<any CHAR except CTLs or separators>
     int tokenEnd = eltEnd;
-    while (tokenEnd < input.length() && isValidTokenChar(input.charAt(tokenEnd))) {
+    while (tokenEnd < input.length() &&
+            isValidTokenChar(input.charAt(tokenEnd))) {
       ++tokenEnd;
     }
 
@@ -135,22 +164,23 @@ public final class Rfc2616AbnfParser {
    * Unqoutes a quoted string.
    *
    * Quoted strings are explained in
-   * <a href="https://tools.ietf.org/html/rfc2616#section-2.2">Section 2.2 of RFC 2616</a>.:
+   * <a href="https://tools.ietf.org/html/rfc2616#section-2.2">Section 2.2 of
+   * RFC 2616</a>:
    *
    * <dl>
    *     <dt>quoted-string</dt>
    *     <dd>A string of text is parsed as a single word if it is quoted using
    *         double-quote marks.
-   *         <pre>
+   * <p><blockquote><pre>
    * quoted-string  = ( &lt;"&gt; *(qdtext | quoted-pair ) &lt;"&gt; )
    * qdtext         = &lt;any TEXT except &lt;"&gt;&gt;
-   *         </pre>
+   * </blockquote></pre>
    *         The backslash character ("\") MAY be used as a single-character
    *         quoting mechanism only within quoted-string and comment
    *         constructs.
-   *         <pre>
+   * <p><blockquote><pre>
    * quoted-pair    = "\" CHAR
-   *         </pre></dd>
+   * </blockquote></pre></dd>
    * </dl>
    *
    * @param str the string to unqoute
